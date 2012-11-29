c531d7b2196c21d4d5cb0a30c06b5db276067e41
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-11-29 20:45:01 +0000

    - Prepped for reports beta

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'storage';
-$app['version'] = '1.4.5';
+$app['version'] = '1.4.6';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
