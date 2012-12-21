c09228e1db1abac3f247f505a268fa0cdd97f47f
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-12-21 02:54:07 +0000

    - Re-enabled for beta build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'storage';
-$app['version'] = '1.4.8';
+$app['version'] = '1.4.10';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-storage.spec
+++ packaging/app-storage.spec
@@ -1,7 +1,7 @@
 
 Name: app-storage
 Epoch: 1
-Version: 1.4.8
+Version: 1.4.10
 Release: 1%{dist}
 Summary: Storage Manager
 License: GPLv3
